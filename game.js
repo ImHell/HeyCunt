aeb64725513a5caed92f26c3551b15a5fe92a811
ImHell <Suicidalscythe@gmail.com>
2016-05-06 10:46:09 +0930

    Create game.js

---
+++ game.js
@@ -0,0 +1,16 @@
+* HTML document, ASCII text, with CRLF line terminators
+<!doctype html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>CLick of the Bananas</title>
+		<style>body {margin: 0;padding: 0;}</style>
+		
+		<script src="lib/phaser.min.js"></script>
+		
+		<script src="src/game.js"></script>
+	</head>
+	<body>
+	
+	</body>
+</html>
